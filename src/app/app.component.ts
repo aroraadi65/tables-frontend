a4cd109e4dec190ac265bec9fb034f0099852764
aroraadi65 <aroraadi65@gmail.com>
2019-06-17 12:06:19 +0530

    table added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { employees } from './../assets/data';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tables-frontend';
+  open = true;
+  employees = employees;
+
+  toggleSidenav() {
+    this.open = !this.open;
+  }
 }
